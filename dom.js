f4de5ddb9a2d0d157343059ce448b1ef5137d133
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-01-18 18:25:29 +0530

    commit changes

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+let headTitle = document.getElementById("header-title");
+//headTitle.textContent="aniket";
+console.log(headTitle);
+let a=document.getElementById("desh");
+a.textContent="ADD ITEM"
+a.style.color="green";
+a.style.fontWeight="bold";
+console.log(a)
---
+++ regform.js
@@ -0,0 +1,17 @@
+
+function signup()
+{
+    const fname=document.getElementById('fname').value;
+    console.log(fname);
+    const email=document.getElementById('email').value;
+    console.log(email);
+    const Phone=document.getElementById('Phone').value;
+    console.log(Phone);
+    const date=document.getElementById('date').value;
+    console.log(date);
+    const time=document.getElementById('time').value;
+    console.log(time);
+}
+
+let k=document.getElementById('desh');
+console.log(k);
