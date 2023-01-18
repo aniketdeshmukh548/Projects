e66d5ffb247f46cac5069b9d94cf59775b5f9f64
Aniket Deshmukh <79415663+aniketdeshmukh548@users.noreply.github.com>
2023-01-18 10:18:00 +0530

    adding the files to project folder

---
+++ regform.js
@@ -0,0 +1,29 @@
+/*function printdata(xyz)
+{
+    var name=document.getElementById("uname").value;
+    let phone=document.getElementById("phone").value;
+    let email=document.getElementById("email").value;
+    let date=document.getElementById("date").value;
+    let time=document.getElementById("time").value;
+    console.log(xyz.target);
+    document.write("your name is :" +name);
+    document.write("your phone is :" +phone);
+    document.write("your email is :" +email);
+    document.write("your date is :" +date);
+    document.write("your time is :" +time);
+}
+//alert('hello');*/
+
+function signup()
+{
+    const fname=document.getElementById('fname').value;
+    console.log(fname);
+    const email=document.getElementById('email').value;
+    console.log(email);
+    const Phone=document.getElementById('Phone').value;
+    console.log(Phone);
+    const date=document.getElementById('date').value;
+    console.log(date);
+    const time=document.getElementById('time').value;
+    console.log(time);
+}
