0e83b67df6c8dcf6784d916126765e287bfbc224
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-01-19 08:59:28 +0530

    added for loop

--- dom.js
+++ dom.js
@@ -13,8 +13,14 @@ console.log(a)
 let b=document.getElementsByClassName("list-group-item");
 console.log(b)
 console.log(b[2])
-b[0].style.fontWeight="bold";
+//by using single item individually'
+/*b[0].style.fontWeight="bold";
 b[1].style.fontWeight="bold";
 b[2].style.fontWeight="bold";
-b[3].style.fontWeight="bold";
+b[3].style.fontWeight="bold";*/
+//for igger list use for loop
+for(let i=0;i<b.length;i++)
+{
+    b[i].style.fontWeight="bold"
+}
 b[2].style.backgroundColor="lightgreen";
