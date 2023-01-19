b4ed55654d1bed93819805614dad39ec15e62630
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-01-19 08:52:56 +0530

    by clss name

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-
+//BY ID
 let headTitle = document.getElementById("header-title");
 //headTitle.textContent="aniket";
 console.log(headTitle);
@@ -7,3 +7,14 @@ a.textContent="ADD ITEM"
 a.style.color="green";
 a.style.fontWeight="bold";
 console.log(a)
+
+//BY CLASS NAME
+
+let b=document.getElementsByClassName("list-group-item");
+console.log(b)
+console.log(b[2])
+b[0].style.fontWeight="bold";
+b[1].style.fontWeight="bold";
+b[2].style.fontWeight="bold";
+b[3].style.fontWeight="bold";
+b[2].style.backgroundColor="lightgreen";
