f4977813d948c4d630625ed0c652cacc964ce163
aniketdeshmukh548 <aniketdeshmukh548@gmail.com>
2023-01-20 17:15:44 +0530

    added object to store to loaclstorage

--- bookingapp.js
+++ bookingapp.js
@@ -1,5 +1,5 @@
 
-function getDetails()
+/* function getDetails()
 {
     var fname=document.getElementById("name").value;
     localStorage.setItem('first name',fname)
@@ -11,4 +11,23 @@ function getDetails()
     localStorage.setItem('DATE',d)
     var t=document.getElementById("time").value;
     localStorage.setItem('TIME',t)
+} */
+
+function getDetails() {
+    var fname = document.getElementById("name").value;
+    var emailadd = document.getElementById("email").value;
+    var phoneno = document.getElementById("phone").value;
+    var d = document.getElementById("date").value;
+    var t = document.getElementById("time").value;
+    let myObj = {
+        name: fname,
+        email: emailadd,
+        phone: phoneno,
+        date: d,
+        time: t,
+    }
+    let myObj_searlized = JSON.stringify(myObj)
+    console.log(myObj_searlized)
+    localStorage.setItem("userdetails", myObj_searlized)
+    console.log(localStorage)
 }
